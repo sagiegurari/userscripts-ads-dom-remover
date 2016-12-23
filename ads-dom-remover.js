f93852bf34c969c829f876388feda809d68a7993
sagiegurari <sagiegurari@gmail.com>
2016-12-23 17:21:42 +0200

    more selectors

--- ads-dom-remover.js
+++ ads-dom-remover.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ads DOM Remover
 // @namespace    sagiegurari
-// @version      1.02
+// @version      1.03
 // @author       Sagie Gur-Ari
 // @description  Removes Ad Containers from DOM (doesn't replace adblocker extension, but blocks dynamic content which the adblocker fails to block by removing whole sections from the HTML DOM.)
 // @homepage     https://github.com/sagiegurari/userscripts-ads-dom-remover
@@ -73,6 +73,7 @@
                 '[class*="GeneralBanner"]',
                 '#vilon',
                 '#prime.shook',
+                '#articlebottomsharinglinks',
                 {
                     selector: '.homepagelitevideo',
                     fineTuneSelector: function ($element) {
