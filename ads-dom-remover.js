23bc79afdc2ceb22464169b890b85dd215362d93
sagie gur ari <sagiegurari@gmail.com>
2017-04-21 08:28:06 +0000

    bump version

--- ads-dom-remover.js
+++ ads-dom-remover.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ads DOM Remover
 // @namespace    sagiegurari
-// @version      1.08
+// @version      1.09
 // @author       Sagie Gur-Ari
 // @description  Removes Ad Containers from DOM (doesn't replace adblocker extension, but blocks dynamic content which the adblocker fails to block by removing whole sections from the HTML DOM.)
 // @homepage     https://github.com/sagiegurari/userscripts-ads-dom-remover
@@ -30,7 +30,7 @@
 // @match        http://*.wikia.com/wiki/*
 // @match        https://*.wikia.com/wiki/*
 // @require      https://code.jquery.com/jquery-2.2.2.min.js
-// @require      https://greasyfork.org/scripts/18490-ads-dom-remover-runner/code/Ads%20DOM%20Remover%20Runner.js?version=18965
+// @require      https://greasyfork.org/scripts/18490-ads-dom-remover-runner/code/Ads%20DOM%20Remover%20Runner.js?version=189663
 // @grant        none
 // @license      MIT License
 // ==/UserScript==
