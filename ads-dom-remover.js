7834393ed7b41eb809fb321a72c046f3ff2dede7
sagie gur ari <sagiegurari@gmail.com>
2022-04-12 06:21:07 +0000

    more selectors

--- ads-dom-remover.js
+++ ads-dom-remover.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ads DOM Remover
 // @namespace    sagiegurari
-// @version      1.35
+// @version      1.36
 // @author       Sagie Gur-Ari
 // @description  Removes Ad Containers from DOM (doesn't replace adblocker extension, but blocks dynamic content which the adblocker fails to block by removing whole sections from the HTML DOM.)
 // @homepage     https://github.com/sagiegurari/userscripts-ads-dom-remover
@@ -105,6 +105,7 @@
                 '#INDbtnWrap',
                 '.YnetPremiumHeaderLogin',
                 '.CreditLogos',
+                '.tp-modal',
                 {
                     selector: '.homepagelitevideo',
                     fineTuneSelector: function ($element) {
