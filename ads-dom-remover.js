67fc748c901963d959394a3e16e0ac0d15a438fb
sagie gur ari <sagiegurari@gmail.com>
2024-08-17 08:50:45 +0000

    fix youtube

--- ads-dom-remover.js
+++ ads-dom-remover.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Ads DOM Remover
 // @namespace    sagiegurari
-// @version      1.37
+// @version      1.38
 // @author       Sagie Gur-Ari
 // @description  Removes Ad Containers from DOM (doesn't replace adblocker extension, but blocks dynamic content which the adblocker fails to block by removing whole sections from the HTML DOM.)
 // @homepage     https://github.com/sagiegurari/userscripts-ads-dom-remover
@@ -185,13 +185,10 @@
         youtube: {
             hostNames: 'youtube.com',
             selectors: [
-                '.ytp-ce-element',
-                'ytd-companion-slot-renderer',
                 '#masthead-ad',
                 '.video-ads ytp-ad-module',
                 '.ytp-ad-overlay-ad-info-dialog-container',
                 '.ytp-ad-overlay-slot',
-                'tp-yt-paper-dialog'
             ],
             options: {
                 loops: 200,
